66819c7c59041edf86582c72ed227ccf3f2ddc63
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-09-25 15:36:15 -0600

    base url changed

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,6 +4,7 @@ import { getToken, logoutService } from './auth'
 
 const axiosInstance = axios.create({
   baseURL: 'http://44.204.25.156:3002/api/',
+  // baseURL: 'http://localhost:3000/api/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
